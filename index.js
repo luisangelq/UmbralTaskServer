e6b23ef133e439cd05554a8d3cde3c3af9982d99
Luis Quiñones <luis.quinones@thebrainapps.com>
2020-10-11 21:58:32 -0500

    more configuration

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(" from server");
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is Working at ${PORT} port`);
+})
